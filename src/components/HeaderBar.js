0096ef7973c1f069b47aa0b88bac259e0c9eb48e
Afeef <afeef9706@gmail.com>
2022-12-30 11:12:48 +0800

    Added icons to heading bar

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -5,7 +5,15 @@ export default class HeaderBar extends React.Component {
     render() {
         return (
             <div className="header-bar" key={this.props.title}>
-                <p>{this.props.title}</p>
+                <p>
+                    {
+                        this.props.title == "Editor" 
+                            ? <i className="fa-solid fa-pen"></i>
+                            : this.props.title == "Markdown Preview"
+                            ? <i className="fa-solid fa-magnifying-glass"></i>
+                            : null
+                    } <span>{this.props.title}</span>
+                </p>
             </div>
         );
     }
